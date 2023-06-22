21297951cd890e72584812aa699def2f9bf2d197
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-22 11:05:20 +0100

    modified the create function of cmd

--- console.py
+++ console.py
@@ -128,7 +128,8 @@ class HBNBCommand(cmd.Cmd):
             try:
                 value = eval(value)
                 if type(value) is str:
-                    value = value.strip('"\'').replace('_', ' ').replace('\\', '"')
+                    value = value.strip('"\'')\
+                        .replace('_', ' ').replace('\\', '"')
                 dictionary[key] = value
             except ValueError as err:
                 pass
@@ -198,7 +199,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -330,6 +331,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
