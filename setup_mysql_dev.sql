986d865a6cd39487aec77b644b341ed3876bb916
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-22 12:43:11 +0100

    added a mysql script to create a new user and database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,7 @@
--- script to create a new user
--- grant some user privileges
-
+-- script to create a new user and grant permissions
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_db_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
